d04084a94b63a237c31cdc37e9b72f309318eaad
Tom <25043847+Douile@users.noreply.github.com>
2020-04-08 17:17:15 +0100

    Add botinfo command

---
+++ src/commands/botinfo.js
@@ -0,0 +1,23 @@
+const { RichEmbed } = require('discord.js');
+
+const Package = require('../../package');
+const DiscordPackage = require('discord.js/package');
+const GameDigPackage = require('gamedig/package');
+
+const call = async function(message) {
+  const client = message.client;
+
+  await message.channel.send(new RichEmbed({
+    title: `${Package.name} status`,
+    description: `[${Package.name} v${Package.version}](${Package.homepage}) [Report bugs here](${Package.bugs.url})\n\
+    Average ping: ${client.ping}ms\n\
+    Uptime: ${client.uptime}ms\n\
+    **Dependencies**\n\
+    [${DiscordPackage.name} v${DiscordPackage.version}](${DiscordPackage.homepage})\n\
+    [${GameDigPackage.name} v${GameDigPackage.version}](${GameDigPackage.homepage})`,
+    timestamp: Date.now()
+  }))
+}
+
+exports.name = 'botinfo';
+exports.call = call;
