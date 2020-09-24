38b6ebda1a10dddacea1f8a618ea4f074f69cef5
Douile <25043847+Douile@users.noreply.github.com>
2020-09-24 23:09:41 +0100

    Remove message cache

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const UPDATE_INTERVALS = {};
 
 const client = new Discord.Client({
   apiRequestMethod: 'sequential',
-  messageCacheMaxSize: 3,
+  messageCacheMaxSize: 0,
   disableEveryone: true,
   restTimeOffset: 1200,
   disabledEvents: [ 'TYPING_START', 'VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE', 'WEBHOOKS_UPDATE' ]
