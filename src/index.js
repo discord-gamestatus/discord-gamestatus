640d29b9cfc626b9889fa00178e5dc4567fa5002
Douile <25043847+Douile@users.noreply.github.com>
2020-09-24 19:59:52 +0100

    Reduce messageMaxCacheMaxSize

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const UPDATE_INTERVALS = {};
 
 const client = new Discord.Client({
   apiRequestMethod: 'sequential',
-  messageCacheMaxSize: 15,
+  messageCacheMaxSize: 3,
   disableEveryone: true,
   restTimeOffset: 1200,
   disabledEvents: [ 'TYPING_START', 'VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE', 'WEBHOOKS_UPDATE' ]
