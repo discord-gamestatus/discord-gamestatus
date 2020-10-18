3e3d80433d32ed99dbe543b56c0c9bb1c24b1a59
Douile <25043847+Douile@users.noreply.github.com>
2020-10-18 20:12:02 +0100

    [usage] Add --support

--- index.js
+++ index.js
@@ -17,21 +17,22 @@ GNU General Public License for more details.
 
 const USAGE = function() {
   console.log('discord-gamestatus: A discord bot that monitors game servers\nhttps://github.com/Douile/discord-gamestatus\n\
-  Usage:\n\
-  \t-d, --debug\t\t\tEnable debug logging\n\
-  \t-v, --verbose\t\t\tEnable verbose logging\n\
-  \t--dev\t\t\t\tEnable dev mode (monitor files for changes and auto-restart)\n\
-  \t-p [prefix], --prefix [prefix]\tChange the command prefix can be any length (--prefix "_")\n\
-  \t--key [key]\t\t\tSet the discord API key (Use of env var recommended)\n\
-  \t--owner [owner]\t\t\tSet the bot owner user ID\n\
-  \t--admin [permissions]\t\tSet the guild permissions required to be bot admin\n\
-  \t--dbl-key [key]\t\t\tSet top.gg API key (Use of env var recommended)\n\
-  \t--tick-count [count]\t\tSet the number of ticks (Each update is assigned a tick so no. ticks is maximum time between a single update)\n\
-  \t--tick-time [time]\t\tSet the time between each tick (in ms)\n\
-  \t--channel-limit [limit]\t\tThe max amount of statuses per channel (0 / not set = infinite)\n\
-  \t--guild-limit [limit]\t\tThe max amount of statuses per guild (0 / not set = infinite)\n\
-  \t--allow-duplicate-updates\tAllow guilds to have multiple statuses for the same IP\n\
-  \t-h, --help\t\t\tShow this help message');
+Usage:\n\
+\t-d, --debug\t\t\tEnable debug logging\n\
+\t-v, --verbose\t\t\tEnable verbose logging\n\
+\t--dev\t\t\t\tEnable dev mode (monitor files for changes and auto-restart)\n\
+\t-p [prefix], --prefix [prefix]\tChange the command prefix can be any length (--prefix "_")\n\
+\t--key [key]\t\t\tSet the discord API key (Use of env var recommended)\n\
+\t--owner [owner]\t\t\tSet the bot owner user ID\n\
+\t--admin [permissions]\t\tSet the guild permissions required to be bot admin\n\
+\t--dbl-key [key]\t\t\tSet top.gg API key (Use of env var recommended)\n\
+\t--tick-count [count]\t\tSet the number of ticks (Each update is assigned a tick so no. ticks is maximum time between a single update)\n\
+\t--tick-time [time]\t\tSet the time between each tick (in ms)\n\
+\t--channel-limit [limit]\t\tThe max amount of statuses per channel (0 / not set = infinite)\n\
+\t--guild-limit [limit]\t\tThe max amount of statuses per guild (0 / not set = infinite)\n\
+\t--allow-duplicate-updates\tAllow guilds to have multiple statuses for the same IP\n\
+\t--support [link]\t\tLink to the support server\n\
+\t-h, --help\t\t\tShow this help message');
 }
 
 const start = require('./src/index.js');
