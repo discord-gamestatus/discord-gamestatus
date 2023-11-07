5aaa4f41f80faee4ec4a940b00ce1ee4d6f31e8a
Douile <douile@douile.com>
2023-11-07 23:29:25 +0000

    tidy: Add missing copyright headers

--- src/structs/LimitedString.ts
+++ src/structs/LimitedString.ts
@@ -1,3 +1,18 @@
+/*
+discord-gamestatus: Game server monitoring via discord API
+Copyright (C) 2023 Douile
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 import { isOfBaseType, truncateString } from "@douile/bot-utilities";
 
 export type LimitedString = (value: unknown) => string;
@@ -13,17 +28,3 @@ export default function limitedString(
     return truncateString(String(value), maxLength, ending);
   };
 }
-
-/*
-export default function LimitedString(maxLength: number, ending?: string): unknown {
-  class LS extends String {
-    constructor(from: unknown) {
-      if (isOfBaseType(from, String)) {
-        from = truncateString(from as string, maxLength, ending);
-      }
-      super(from);
-    }
-  }
-  return LS;
-}
-*/
